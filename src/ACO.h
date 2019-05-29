31a36aa7d109025ae21116176d48d26e1f4d89dc
Xenakis <xenakiskaramanos@gmail.com>
2019-05-29 17:51:45 +0300

    Add member variables and set parameters function

--- src/ACO.h
+++ src/ACO.h
@@ -13,11 +13,29 @@ public:
 
   std::vector<int> getBestPath();
   double getBestLength();
+
+  void setParameters(double maxIterations = 3000,
+					  double numAnts = 20,
+					  double explorationProbability = 0.9,
+					  double exhaustRatePheromones = 0.1,
+					  double transitionProbabilityB = 2,
+					  double transitionProbabilityX = 0.1,
+					  double transitionProbabilityA = 1);
+
   void RunACS(std::string DistancesFilename);
 
 private:
   std::vector<std::vector<double> > _distances;
   std::vector<std::vector<double> > _destinations;
+
+  // ACO Parameters
+  double _maxIterations = 3000;
+  double _numAnts = 20;
+  double _explorationProbability = 0.9;
+  double _exhaustRatePheromones = 0.1;
+  double _transitionProbabilityB = 2;
+  double _transitionProbabilityX = 0.1;
+  double _transitionProbabilityA = 1;
 
   int _width;
   int _heigth;
